ec90b6f878e14ecb5bdc82eb2e26e5b86c8f2196
niuiic <niuiicde@gmail.com>
2025-06-20 11:18:40 +0800

    chore: config

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { chromium } from 'playwright'
 
-const wsUrl =
-  'ws://127.0.0.1:9222/devtools/page/927D024659F56E58CF8C41391E462EA5'
-const browser = await chromium.connectOverCDP(wsUrl)
-console.log(browser)
+const browser = await chromium.connectOverCDP('http://127.0.0.1:9222')
+
+const page = await browser.newPage({
+  viewport: { width: 1920, height: 1080 },
+  deviceScaleFactor: 2
+})
+await page.goto('https://www.baidu.com', { waitUntil: 'networkidle' })
+await page.screenshot({
+  fullPage: true,
+  path: './output/test.png',
+  scale: 'device'
+})
+
+await browser.close()
