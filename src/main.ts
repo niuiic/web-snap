6a8483a4d9cbfa7d5962949af24591b7c174a52b
niuiic <niuiicde@gmail.com>
2025-07-04 15:02:04 +0800

    chore: deps

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,5 @@ import { chromium } from 'playwright'
 
 const browser = await chromium.connectOverCDP('http://127.0.0.1:9222')
 
-const page = await browser.newPage({
-  viewport: { width: 1920, height: 1080 },
-  deviceScaleFactor: 2
-})
-await page.goto('https://www.baidu.com', { waitUntil: 'networkidle' })
-await page.screenshot({
-  fullPage: true,
-  path: './output/test.png',
-  scale: 'device'
-})
-
-await browser.close()
+const page = await browser.newPage()
+await page.goto('https://www.baidu.com')
