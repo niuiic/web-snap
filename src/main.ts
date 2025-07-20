de91e0cfe70c47c173733245f0c16602fe5e2bb2
niuiic <niuiicde@gmail.com>
2025-07-20 20:49:58 +0800

    feat: stealth

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-import { chromium } from 'playwright'
+import { chromium } from 'playwright-extra'
+import stealth from 'puppeteer-extra-plugin-stealth'
+
+chromium.use(stealth())
 
 const browser = await chromium.connectOverCDP('http://127.0.0.1:9222')
 const page = await browser.newPage()
+
 await page.goto(
-  'https://learn.lianglianglee.com/%E4%B8%93%E6%A0%8F/10x%E7%A8%8B%E5%BA%8F%E5%91%98%E5%B7%A5%E4%BD%9C%E6%B3%95'
+  'https://learn.lianglianglee.com/%E4%B8%93%E6%A0%8F/Kafka%E6%A0%B8%E5%BF%83%E6%8A%80%E6%9C%AF%E4%B8%8E%E5%AE%9E%E6%88%98/00%20%E5%BC%80%E7%AF%87%E8%AF%8D%20%E4%B8%BA%E4%BB%80%E4%B9%88%E8%A6%81%E5%AD%A6%E4%B9%A0Kafka%EF%BC%9F.md'
 )
+await page.waitForTimeout(1e3)
 await page.pdf({ path: './output/learn.pdf' })
 await page.close()
+await browser.close()
