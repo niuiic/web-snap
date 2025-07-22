4ae1ecdd6e2aae595ce4d65f684f013270c7e377
niuiic <niuiicde@gmail.com>
2025-07-22 18:29:25 +0800

    feat: remove stealth

--- src/browser.ts
+++ src/browser.ts
@@ -1,9 +1,4 @@
 import { chromium } from 'playwright'
 
-// import { chromium } from 'playwright-extra'
-// import stealth from 'puppeteer-extra-plugin-stealth'
-//
-// chromium.use(stealth())
-
 export const getBrowser = (cdpURL: string = 'http://127.0.0.1:9222') =>
   chromium.connectOverCDP(cdpURL)
